02208da6609bd9c6b20706c942288f1e1bdf43f4
Isaiah Patton <isaiahpatton1@gmail.com>
2016-04-17 14:49:20 -0400

    Update face.js

--- js/face.js
+++ js/face.js
@@ -1,6 +1,6 @@
 function nedface() {
     var image = document.getElementById('myImage');
-    if (image.src.match("https://isaiahpatton.github.io/ned/imgs/ned.gif")) {
+    if (image.src.match("http://isaiahpatton.github.io/ned/imgs/ned.gif")) {
         image.src = "http://isaiahpatton.github.io/ned/imgs/ned.jpg";
     } else {
         image.src = "http://isaiahpatton.github.io/ned/imgs/ned.gif";
