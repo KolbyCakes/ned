92c85bcbe21d2a5cce7840a791c8f1bda5f7b8c8
Isaiah Patton <isaiahpatton1@gmail.com>
2016-05-19 17:41:30 -0400

    Create settings.cs

---
+++ stylesheets/settings.cs
@@ -0,0 +1,15 @@
+nedface, nedsettings {
+    color: blue;
+    text-decoration: underline;
+    cursor: pointer;
+}
+nedsettings#inside {
+    color: black;
+    text-decoration: none;
+    cursor: none;
+}
+nedsettings#insideText {
+    display: none; <!--So it does not display when disabled-->
+    border-style: solid;
+    border-color: lightgray;
+}
