631c490665b0bfe10e7e46f8c41364f2a8c71687
Isaiah Patton <isaiahpatton1@gmail.com>
2016-05-21 09:04:13 -0400

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -1,7 +1,5 @@
 function backgroundon() {
-document.getElementById("bg").innerHTML = '<style type="text/css" id="bg">
-html, body, bg, background {background-image: url("http://enjoycss.com/webshots/hB_1.png");}
-</style>';
+document.getElementById("bg").innerHTML = '<style type="text/css" id="bg"> html, body, bg, background {background-image: url("http://enjoycss.com/webshots/hB_1.png");} </style>';
 document.getElementById("bgenable").innerHTML = '';
 document.getElementById("bgdisable").innerHTML = '<nedface id="bgdisable" onclick="backgroundoff()">Disable Background</nedface>';
 }
