cc67a00f479f2baee8250dc83c68fd52514893af
Isaiah Patton <isaiahpatton1@gmail.com>
2016-05-21 09:03:44 -0400

    Create bg.js

---
+++ js/bg.js
@@ -0,0 +1,13 @@
+function backgroundon() {
+document.getElementById("bg").innerHTML = '<style type="text/css" id="bg">
+html, body, bg, background {background-image: url("http://enjoycss.com/webshots/hB_1.png");}
+</style>';
+document.getElementById("bgenable").innerHTML = '';
+document.getElementById("bgdisable").innerHTML = '<nedface id="bgdisable" onclick="backgroundoff()">Disable Background</nedface>';
+}
+
+function backgroundoff() {
+document.getElementById("bg").innerHTML = '';
+document.getElementById("bgenable").innerHTML = '<nedface id="bgenable" onclick="backgroundon()">Enable Background</nedface>';
+document.getElementById("bgdisable").innerHTML = '';
+}
