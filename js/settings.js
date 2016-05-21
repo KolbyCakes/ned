b0ffa511f4b77baed8b8d9d68e03d7d715e96c2f
Isaiah Patton <isaiahpatton1@gmail.com>
2016-05-21 08:57:38 -0400

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -23,3 +23,19 @@ document.getElementById("inside").innerHTML = document.getElementById("insideTex
 document.getElementById("hbox").innerHTML = '<nedface id="hbox" onclick="hbox()">Hide Settings</nedface>';
 document.getElementById("sbox").innerHTML = '';
 }
+
+function backgroundon() {
+document.getElementById("bg").innerHTML = '    <style type="text/css" id="bg">
+html, body, bg, background {
+  background-image: url("http://enjoycss.com/webshots/hB_1.png");
+}
+</style>';
+document.getElementById("bgenable").innerHTML = '';
+document.getElementById("bgdisable").innerHTML = '<nedface id="bgdisable" onclick="backgroundoff()">Disable Background</nedface>';
+}
+
+function backgroundoff() {
+document.getElementById("bg").innerHTML = '';
+document.getElementById("bgenable").innerHTML = '<nedface id="bgenable" onclick="backgroundon()">Enable Background</nedface>';
+document.getElementById("bgdisable").innerHTML = '';
+}
