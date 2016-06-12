725234fb804afa376f4ec361c27fe5e34bad32cc
Isaiah James Patton <isaiahpatton1@gmail.com>
2016-06-12 19:53:17 -0400

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -10,6 +10,7 @@ function footer() {
     document.write(' </small>');
     document.write('</ul>');
     document.write('<!--Fork on Github--><a class="github-button" href="https://github.com/n-e-d/ned/fork" data-style="mega" data-count-href="/n-e-d/ned/network" data-count-api="/repos/n-e-d/ned#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork ned on GitHub">Fork</a>');
+    document.write('&nbsp;')
     document.write('<!--Report bug on Github--><a class="github-button" href="https://github.com/n-e-d/ned/issues" data-icon="octicon-issue-opened" data-style="mega" data-count-api="/repos/n-e-d/ned#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue ned on GitHub">Report Issue</a></p>')
     document.write('    <script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script></footer>');
 }
@@ -27,6 +28,8 @@ function nedsettings() {
     document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/bg.js"></script>');
     document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/settings.js"></script>');
     document.write('<br>');
+}
+function outhersites() {
     document.write('<nedpages>');
     document.write('  <a href="http://nedhome.ml" class="active">Ned</a>');
     document.write('  <a href="http://isaiahpatton.github.io/monopoly">HTML Monopoly</a>');
