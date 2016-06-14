acb00ea1763c5da0ff7b519f9afdffca0af56ad4
Isaiah James Patton <isaiahpatton1@gmail.com>
2016-06-14 14:38:27 -0400

    Create AllSites.js

---
+++ js/AllSites.js
@@ -0,0 +1,24 @@
+function AllNedTools() {
+   NedTool("Calculator", "nedhome.mlnedhome.ml/tools/calculator", "www.free-icons-download.net/images/calculator-logo-73002.png");
+   
+   NedTool("Calander", "nedhome.mlnedhome.ml/tools/calendar", "goo.gl/hGkTvi")
+   
+   NedTool("Clock", "nedhome.ml/tools/clock", "goo.gl/V4Zwrh");
+   
+   NedTool("$ Converter", "nedhome.ml/tools/moneyconverter", "goo.gl/g0vTnM");
+}
+function AllOutherSites() {
+   OutherSite("SUSEStudio", "susestudio.com", "goo.gl/64ozgA");
+   
+   OutherSite("Weebly", "weebly.com", "upload.wikimedia.org/wikipedia/commons/4/43/Weebly_logo_2013.png");
+   
+   OutherSite("Youtube", "youtube.com", "t2.gstatic.com/images?q=tbn:ANd9GcQDfL7X2SHlu_A9qhVi4HhJFMFqkX4VBAZL9LS81bJm_RatOoM71KJX9nv3");
+   
+   OutherSite("Linux", "linux.com", "upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1000px-Tux.svg.png");
+   
+   OutherSite("CCleaner", "ccleaner.com", "upload.wikimedia.org/wikipedia/en/4/4a/CCleaner_logo_2013.png");
+}
+function AllSites() {
+   AllNedTools();
+   AllOutherSites();
+}
