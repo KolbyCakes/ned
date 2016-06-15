02ad4da8633ae84d6ca2e3f8223af3793bec48d3
Isaiah James Patton <isaiahpatton1@gmail.com>
2016-06-14 20:45:24 -0400

    Update Sites.js

--- js/Sites.js
+++ js/Sites.js
@@ -1,7 +1,7 @@
 function Button(name, url, type, img) {
     document.write('<a class="oldbutton" href="http://' + url + '">' + name + '<br>');
     document.write('<small class="extra2">' + type + '</small><br>');
-    document.write('<img src="http://' + img + '" alt="' + name + '" width="90" height="90">');
+    document.write('<img src="http://' + img + '" alt="' + name + '" width="88" height="75">');
     document.write('</a> &nbsp;');
 }
 function NedGames() {
