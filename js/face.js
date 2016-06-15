fc4af45e45bd17909ef3a3f5cdb742aee4f42841
Isaiah James Patton <isaiahpatton1@gmail.com>
2016-06-14 20:53:21 -0400

    Update face.js

--- js/face.js
+++ js/face.js
@@ -1,7 +1,12 @@
 function nedface() {
     var image = document.getElementById('myImage');
     if (image.src.match("http://i.imgur.com/AP5imrD.png")) {
-        image.src = "http://isaiahpatton.github.io/ned/imgs/ned.gif";
+        document.write('<CENTER><h2 style="color:white">Ned News!</h2></CENTER>');
+        document.write('<nednews syle="color:white">');
+        document.write('Welcome to the brand new NedNews!');
+        document.write('');
+        document.write('</nednews>');
+        //image.src = "http://n-e-d.github.io/ned/imgs/ned.gif";
     } else {
         image.src = "http://i.imgur.com/AP5imrD.png";
     }
