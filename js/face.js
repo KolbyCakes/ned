d47a59b22b8a533993e2c2792246a49f1995b665
Isaiah James Patton <isaiahpatton1@gmail.com>
2016-06-16 12:28:09 -0400

    Update face.js

--- js/face.js
+++ js/face.js
@@ -14,12 +14,12 @@ function nedface() {
 function faceHtml() {
     document.write('<div class="inner">')
     document.write('<!--Ned Face--><CENTER>')
-    document.write('<!--<img id="myImage" src="http://i.imgur.com/AP5imrD.png" onclick="nedface()" alt="Ned's Face" height="200" width="600"><br>-->')
-    document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/settings.js"></script>')
-    document.write('<p id="demo">')
-    document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/settings.js"></script>')
-    document.write('<!--JavaScript change Ned face-->')
-    document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/face.js"></script>')
-    document.write('</CENTER>')
-    document.write('</div>')
+    document.write('<!--<img id="myImage" src="http://i.imgur.com/AP5imrD.png" onclick="nedface()" alt="Ned's Face" height="200" width="600"><br>-->');
+    document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/settings.js"></script>');
+    document.write('<p id="demo">');
+    document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/settings.js"></script>');
+    document.write('<!--JavaScript change Ned face-->');
+    document.write('<script type="text/javascript" src="http://n-e-d.github.io/ned/js/face.js"></script>');
+    document.write('</CENTER>');
+    document.write('</div>');
 }
