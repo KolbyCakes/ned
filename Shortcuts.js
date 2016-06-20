42c75777b6482bc82cf762ad316160c79c753cea
Isaiah James Patton <isaiahpatton1@gmail.com>
2016-06-20 13:07:28 -0400

    Create Shortcuts.js

---
+++ Shortcuts.js
@@ -0,0 +1,14 @@
+function Write(text) {
+    document.write(text)
+}
+function URL() {
+    window.location.href;
+}
+function SetURL(url) {
+    if (url == "https://nedhome.ml") {
+      url == "http://nedhome.ml"
+      window.location.href == "http://nedhome.ml"
+    } else {
+      window.location.href == url
+    }
+}
