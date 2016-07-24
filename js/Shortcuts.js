8b8b7f1c8e893d0057bed0f4d2ef00eb59b3e094
Isaiah Patton <isaiahpatton1@gmail.com>
2016-07-24 17:41:37 -0400

    Update and rename Shortcuts.js to js/Shortcuts.js

---
+++ js/Shortcuts.js
@@ -0,0 +1,15 @@
+function write(text) {
+    document.write(text);
+}
+function URL() {
+    window.location.href;
+}
+function isURL(check) {}
+function SetURL(url) {
+    if (url == "https://nedhome.ml") {
+      url == "http://nedhome.ml";
+      window.location.href == "http://nedhome.ml";
+    } else {
+      window.location.href == url;
+    }
+}
