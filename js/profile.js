ebccb25f61583fd033966a39a1fa983cd32d0945
Isaiah Patton <isaiahpatton1@gmail.com>
2016-07-24 09:49:18 -0400

    Create profile.js

---
+++ js/profile.js
@@ -0,0 +1,9 @@
+function profile() {
+  document.write("&nbsp; &nbsp;");
+  checkCookie();
+  document.write("&nbsp; &nbsp;");
+  <document.write('<small class="extra2">');
+  <document.write('  <nedprofile style="color:blue; text-decoration:underline;" onclick="gotoProfile()">[Profile]&nbsp;</nedprofile>');
+  <document.write('  <redoname style="color:blue; text-decoration:underline;" onclick="resetName()">[Logout]</redoname>');
+  <document.write('</small>');
+}
