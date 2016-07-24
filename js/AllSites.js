04fffd0bb49f3cac50d21df8bfa0ef9a986109bf
Isaiah Patton <isaiahpatton1@gmail.com>
2016-07-24 17:05:03 -0400

    Add files via upload

--- js/AllSites.js
+++ js/AllSites.js
@@ -1,15 +1,34 @@
+/*
+How this works:
+    this takes what you write and change it into HTML code (see Sites.js for more info)
+How to use:
+    NedTool("Name", "Link to site", "Link to image"); <-- This has been replaced (for the most part) with DNedTool
+    DNedTool("Name", "Link to image"); <-- This is a direct version of NedTool where the name is taken and the site is set to nedhome.ml/tools/name  
+    OutherSite("Name", "Link to site", "Link to image"); <-- Link to outsite site thats not part of the Ned sites
+    SocialMedia("Name", "Link to site", "Link to image"); <-- Facebook, Twitter, ex...
+
+    All of the stuff above is a shortcut to Button() ex NedTool() whould be this:
+        Button("Name", "url", "NedTool", "image");
+*/
+
 function AllNedTools() {
-   NedTool("Google", "nedhome.ml/tools/google", "goo.gl/b274CE");
+   DNedTool("google", "goo.gl/b274CE");
    
    NedTool("Yahoo", "nedhome.ml/tools/google?yahoo", "goo.gl/Tu73C3");
    
-   NedTool("Calculator", "nedhome.ml/tools/calculator", "www.free-icons-download.net/images/calculator-logo-73002.png");
+   DNedTool("Calculator", "www.free-icons-download.net/images/calculator-logo-73002.png");
    
-   NedTool("Calander", "nedhome.ml/tools/calendar", "goo.gl/hGkTvi")
+   DNedTool("Calendar", "goo.gl/hGkTvi")
    
-   NedTool("Clock", "nedhome.ml/tools/clock", "goo.gl/V4Zwrh");
+   DNedTool("Clock", "goo.gl/V4Zwrh");
    
    NedTool("$ Converter", "nedhome.ml/tools/moneyconverter", "goo.gl/g0vTnM");
+   
+   DNedTool("Holidays", "pixabay.com/static/uploads/photo/2015/12/10/17/46/fir-1086772_960_720.png");
+   
+   DNedTool("Radio", "upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Icon_sound_radio.svg/1259px-Icon_sound_radio.svg.png");
+   
+   DNedTool("Tv", "pixabay.com/static/uploads/photo/2016/03/31/15/23/icon-1293234_960_720.png");
    
    NedGames();
 }
@@ -25,12 +44,29 @@ function AllOutherSites() {
    OutherSite("CCleaner", "ccleaner.com", "upload.wikimedia.org/wikipedia/en/4/4a/CCleaner_logo_2013.png");
    
    OutherSite("Microsoft", "microsoft.com", "img05.deviantart.net/e1f7/i/2011/251/e/4/microsoft_windows_logo_3000px_by_davidm147-d3hax3m.png");
+   
+   OutherSite("Wikipedia", "wikipedia.org", "upload.wikimedia.org/wikipedia/commons/7/7a/Nohat-wiki-logo.png");
+   
+   OutherSite("W3 Schools", "w3schools.com", "w3schools.com/favicon.ico");
 }
 function AllSocialMedia() {
    SocialMedia("Facebook", "facebook.com", "upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/2000px-F_icon.svg.png");
    
    SocialMedia("Twitter", "twitter.com", "upload.wikimedia.org/wikipedia/en/thumb/9/9f/Twitter_bird_logo_2012.svg/220px-Twitter_bird_logo_2012.svg.png");
+   
+   SocialMedia("Flickr", "flickr.com", "upload.wikimedia.org/wikipedia/commons/9/9b/Flickr_logo.png");
+   
+   SocialMedia("Google+", "plus.google.com", "upload.wikimedia.org/wikipedia/commons/1/13/Google%2Bapp_icon.png")
 }
+function AllKids() {
+    Kids("Disney", "disney.com", "upload.wikimedia.org/wikipedia/commons/4/4c/Walt_Disney_Pictures.png");
+    Kids("Nick", "nick.com", "upload.wikimedia.org/wikipedia/commons/8/85/Nick_(Logo).png");
+    Kids("CN", "cartoonnetwork.com", "bit.ly/CNLogo"); //The image is from Wikipedia just shortened
+    Kids("Safe Google", "nedhome.ml/tools/google?kids", "www.edu.pe.ca/lmmontgomery/Links/images/kid_rex.jpg");
+    Kids("GamesFreak", "gamesfreak.net", "static.gamepilot.com/i/c75_themes/gamesfreak/fast-freddy-blue-pale.jpg");
+    Kids("Minecraft", "minecraft.net", "alice.violympic.vn/assets/uploads/member/icon_minecraft.png");
+}
+
 function AllSites() {
    AllNedTools();
    
@@ -38,6 +74,10 @@ function AllSites() {
    
    AllOutherSites();
    
+   NewLine("br");
+   
+   AllKids();
+
    NewLine("br");
    
    AllSocialMedia();
--- js/Sites.js
+++ js/Sites.js
@@ -1,9 +1,12 @@
 function Button(name, url, type, img) {
+    name0 = name.charAt(0);
+    name1 = name0.toUpperCase();
+    name2 = name.replace(name0, name1);
     document.write('<!--' + name +'-->')
-    document.write('<a class="oldbutton" href="http://' + url + '">' + name + '<br>');
-    document.write('<small class="extra2">' + type + '</small><br>');
-    document.write('<img src="http://' + img + '" alt="' + name + '" width="88" height="75">');
-    document.write('</a> &nbsp;');
+    document.write('    <a class="oldbutton" href="http://' + url + '">' + name2 + '<br>');
+    document.write('       <small class="extra2">' + type + '</small><br>');
+    document.write('        <img src="http://' + img + '" alt="' + name2 + '" width="88" height="75">');
+    document.write('    </a> &nbsp;');
 }
 function NedGames() {
     Button("NedGames", "games.nedhome.ml", "NedGames", "nedhome.ml/imgs/games.png");
@@ -14,16 +17,25 @@ function B(name, url, type, img) {
 function NedTool(name, url, img) {
     Button(name, url, "NedTool", img)
 }
+function DNedTool(name, img) {
+    // If the name of the nedtool is the same as the part after nedhome.ml/tools/ then use this
+    NedTool(name, "nedhome.ml/tools/" + name.toLowerCase(), img)
+}
 function OutherSite(name, url, img) {
     Button(name, url, "Outher Site", img)
 }
 function SocialMedia(name, url, img) {
     Button(name, url, "Social Media", img);
 }
+function Kids(name, url, img) {
+    Button(name, url, "For Kids", img);
+}
 function NewLine(type) {
     if(type == "p") {
         document.write("</p>")
     } else if(type == "br") {
+        document.write("<br>")
+    } else if(type == null || type == "") {
         document.write("<br>")
     }
 }
